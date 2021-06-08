47c803980caf512801231ce21f59a2911f377e2b
Vyctor <dev.vyctor@gmail.com>
2021-06-07 21:19:16 -0300

    feat: criando service de especificacao e separando em modulos

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from '../model/category.model';
+import { Category } from '../model/Category';
 import { ICategoriesRepository, ICreateCategoryDTO } from './ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
