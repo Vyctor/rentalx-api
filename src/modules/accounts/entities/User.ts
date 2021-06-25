22237ebcb6106a1759273eed6d9fa7d81b55a5f4
Vyctor <dev.vyctor@gmail.com>
2021-06-24 21:13:32 -0300

    criando migration de usuario

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,30 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  public id: string;
+  @Column()
+  public name: string;
+  @Column()
+  public username: string;
+  @Column()
+  public password: string;
+  @Column()
+  public email: string;
+  @Column()
+  public driver_license: string;
+  @Column()
+  public isAdmin: boolean;
+  @CreateDateColumn()
+  public createdAt: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { User };
