df79d3c3f0e835c7232672c16978957882ce70a7
Vyctor <dev.vyctor@gmail.com>
2021-06-26 23:18:11 -0300

    avatar de usuario

--- src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from 'tsyringe';
 
+import { deleteFile } from '../../../../utils/file';
 import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
@@ -16,8 +17,11 @@ class UpdateUserAvatarUseCase {
   public async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
 
-    user.avatar = avatar_file;
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
+    user.avatar = avatar_file;
     await this.usersRepository.create(user);
   }
 }
