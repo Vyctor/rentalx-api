5afcd3206e325c3830f2c28d05ebbfddd66bc1d8
Vyctor <dev.vyctor@gmail.com>
2021-07-31 18:55:54 -0300

    continuando o caso de uso de carros

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { InMemoryCarsRepository } from '@modules/cars/repositories/in-memory/inMemoryCarsRepository';
 
+import { AppError } from '../../../../shared/errors/AppError';
 import { CreateCarUseCase } from './CreateCarUseCase';
 
 let createCarUseCase: CreateCarUseCase;
@@ -12,7 +13,7 @@ describe('CreateCar', () => {
   });
 
   it('should be able to create a new car', async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: 'Name car',
       description: 'Description Car',
       daily_rate: 100,
@@ -21,5 +22,45 @@ describe('CreateCar', () => {
       brand: 'Brand',
       category_id: 'Category',
     });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('should not be able to create a car that already registered with same license plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Name car',
+        description: 'Description Car',
+        daily_rate: 100,
+        license_plate: 'ABC-1235',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car 2',
+        description: 'Description Car',
+        daily_rate: 100,
+        license_plate: 'ABC-1235',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to create a car with available true by default', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Available',
+      description: 'Description Car',
+      daily_rate: 100,
+      license_plate: 'ABD-1235',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category',
+    });
+
+    expect(car.available).toBe(true);
   });
 });
