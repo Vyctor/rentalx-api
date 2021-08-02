93aa0aea5d162b1e0275e1d699386bf3ab14e42b
Vyctor <dev.vyctor@gmail.com>
2021-08-02 00:40:25 -0300

    criando middleware de administrador

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ensureAuthentication } from '@shared/infra/http/middlewares/ensureAuthentication';
+
+import { ensureAdmin } from '../middlewares/ensureAdmin';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 
-carsRoutes.post('/', createCarController.handle);
+carsRoutes.post('/', ensureAuthentication, ensureAdmin, createCarController.handle);
 
 export { carsRoutes };
