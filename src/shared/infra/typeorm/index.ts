0a0cfdd28ce88503cc40cf4516d29bd215893190
Vyctor <dev.vyctor@gmail.com>
2021-08-02 00:18:18 -0300

    corrigindo problemas da aula anterior

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,10 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'database'): Promise<Connection> => {
+export default async (host = 'database_ignite'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   return createConnection(
     Object.assign(defaultOptions, {
-      host: host || 'database',
+      host: host || 'database_ignite',
     }),
   );
 };
