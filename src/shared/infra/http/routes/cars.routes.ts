184aa956aa127b08fd865944a0e782c2e8864688
Vyctor <dev.vyctor@gmail.com>
2021-08-06 19:37:14 -0300

    caso de uso do cadastro de imagens do carro

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '@config/upload';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListaAvailableCarsController';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController';
 import { ensureAuthentication } from '@shared/infra/http/middlewares/ensureAuthentication';
 
 import { ensureAdmin } from '../middlewares/ensureAdmin';
@@ -12,11 +15,16 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload('./tmp/cars'));
 
 carsRoutes.post('/', ensureAuthentication, ensureAdmin, createCarController.handle);
 
 carsRoutes.get('/available', listAvailableCarsController.handle);
 
 carsRoutes.post('/specifications/:id', ensureAuthentication, ensureAdmin, createCarSpecificationController.handle);
+
+carsRoutes.post('/images', ensureAuthentication, ensureAdmin, upload.array('images'), uploadCarImagesController.handle);
 
 export { carsRoutes };
