5b5eb7cca06125e712c923205d5c4fd9a1feb656
Vyctor <dev.vyctor@gmail.com>
2022-01-13 16:16:24 -0300

    upload utilizando s3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -7,9 +7,15 @@ import { IStorageProvider } from '@shared/container/providers/StorageProvider/IS
 import { IMailProvider } from './MailProvider/IMailProvider';
 import { EtherealMailProvider } from './MailProvider/implementations/EtherealMailProvider';
 import { LocalStorageProvider } from './StorageProvider/implementations/LocalStorageProvider';
+import { S3StorageProvider } from './StorageProvider/implementations/S3StorageProvider';
 
 container.registerSingleton<IDateProvider>('DayjsDateProvider', DayJsDateProvider);
 
 container.registerSingleton<IMailProvider>('EtherealMailProvider', EtherealMailProvider);
 
-container.registerSingleton<IStorageProvider>('StorageProvider', LocalStorageProvider);
+const diskStorage = {
+  local: LocalStorageProvider,
+  s3: S3StorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>('StorageProvider', diskStorage[process.env.DISK]);
