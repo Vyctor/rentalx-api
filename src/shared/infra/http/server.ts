9be231e919fb8da4b924d530c44206d2d0860613
Vyctor <dev.vyctor@gmail.com>
2022-01-17 15:05:16 -0300

    adicionando porta na qual roda a api no .env

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,3 @@
 import { app } from './app';
 
-app.listen(3333, () => console.log('Server is running at => ', new Date()));
+app.listen(process.env.API_PORT, () => console.log('Server is running at => ', process.env.API_PORT));
